c87e3dffde4956e003faf4903cbf69ed3459fcba
gowdaganesh952-cmyk <gowdaganesh952@gmail.com>
2026-08-17 19:14:18 +0530

    error coeee

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,6 +3,7 @@ import HomePageReady from "./components/HomePageReady";
 import FeaturedProducts from "./components/FeaturedProducts";
 import Footer from "./components/Footer";
 
+
 export default function HomePage() {
   return (
     <main className="min-h-screen w-full overflow-x-hidden bg-white text-black">
